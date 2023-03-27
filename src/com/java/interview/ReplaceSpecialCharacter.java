d93fae4c0d0ef542849e596a30ff5f14f339e80f
ReenaRavichandran <reenaravichandran16@gmail.com>
2023-03-27 11:51:04 +0530

    updated programs

--- src/com/java/interview/ReplaceSpecialCharacter.java
+++ src/com/java/interview/ReplaceSpecialCharacter.java
@@ -5,6 +5,7 @@ public class ReplaceSpecialCharacter {
 	
 	private static void subMethod() {
 		String s = "This&String@Contains*Special^Characters%";
+		
 		String r = s.replaceAll("[^a-zA-Z0-9]", " ");
 		System.out.println(r);
 	}
